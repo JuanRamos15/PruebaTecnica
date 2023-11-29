700a9b8a693cdb26901d8eec11d7d7afa1abf828
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-11-28 23:54:58 -0600

    🌟enhance: Se agregaron comentarios y formatos
    Se agregaron comentarios en las vistas y script para una mejor lectura de los mismos, asi como formato a vistas y scripts para darle un formato limpio

--- scripts/mensajes.js
+++ scripts/mensajes.js
@@ -3,17 +3,14 @@ function mostrarAlerta() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Obtén el formulario y agrega un evento submit
   var form = document.querySelector("form");
   form.addEventListener("submit", function (event) {
-    // Obtén el valor del campo de correo
     var email = document.getElementById("email").value;
 
     // Verifica si el campo de correo está lleno
     if (email.trim() === "") {
-      // Evita que se envíe el formulario si el campo está vacío
       event.preventDefault();
-      alert("Por favor, ingresa tu correo antes de recuperar la contraseña");
+      
     } else {
       // Llama a la función mostrarAlerta solo si el campo de correo está lleno
       mostrarAlerta();
